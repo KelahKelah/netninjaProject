5c64f86df8709777f8a11d5c3cfa499df1544209
KelahKelah <kelechiekebor@gmail.com>
2020-03-03 15:27:23 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navigation from '../src/components/navigation/navigation'
 import './App.css';
-// import Form from './components/form/form';
-import Todo from './components/todo/todo';
+import {BrowserRouter, Route } from 'react-router-dom'
+import Home from '../src/page/home/home'
+import Contact from '../src/page/contact/contact'
+import Logout from '../src/page/logout/logout'
 
-function App() {
-  return (
-    <div>
-      <Form />
-      <Todo />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="">
+          <Navigation />
+          <Route exact path='/' component={Home} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/logout' component={Logout} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import TodoList from './todoList';
+import AddTodo from '../todo/addTodo';
 
 class Todo extends Component {
     constructor(props) {
@@ -9,14 +10,33 @@ class Todo extends Component {
                 {id:1, title: 'Repair my Phones charging port', status: true},
                 {id:2, title: 'Buy airtel mifi', status: true},
                 {id:3, title: 'Buy insence', status: true},
+                {id:3, title: 'Give crucifix in church', status: true},
+                {id:3, title: 'Go for saturday work', status: true},
             ]
-        }
+        }  
     }
+    deleteTodo = (id) =>  {
+        // console.log(id)  
+    const deleteMyTodos = this.state.myTodos.filter(todo => {
+    return todo.id !== id });
+    this.setState({todos: deleteMyTodos})
+    }
+    addTodo = (event) => {
+        // console.log(id)
+    }
+    componentDidMount() {
+        console.log('component mouted')
+    }
+    componentDidUpdate(preProps, prevState) {
+        console.log('component updated');  
+    }
+
     render() {
-         console.log(this.state.myTodos)
+        //  console.log(this.state.myTodos)
         return(
             <div>
-                <TodoList myTodos={this.state.myTodos} />
+                <AddTodo addTodo={this.addTodo} />
+                <TodoList myTodos={this.state.myTodos} deleteTodo={this.deleteTodo} />
             </div>
         )
     }
