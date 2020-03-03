5d25b5c61096f2975400d223a231bd511a50cf3d
KelahKelah <kelechiekebor@gmail.com>
2020-03-03 15:12:37 +0100

    rearrange paths and add  navigation routes

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+var myStyle = {
+    // backgroundColor: '#cc0000',
+    color: '#f2f2f2',
+    padding: '1%'
+}
+
+function Header() {
+    return(
+        <div>
+            <h4 style={myStyle}>Todo</h4>
+        </div>
+    )
+}
+export default Header;
